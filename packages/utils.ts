ea472ee56aef7d98badc5cb76d9c1f3a64a639c0
Desmond <shuguang-lv@outlook.com>
2024-02-05 11:57:35 -0800

    feat: localStorage

--- packages/utils.ts
+++ packages/utils.ts
@@ -1496,3 +1496,28 @@ export function randomSort2(arr: any[]) {
 
   return arr
 }
+
+// 支持过期时间的 localStorage
+;(function () {
+  localStorage.setItem = function (key, value, time = Infinity) {
+    const payload = Number.isFinite(time)
+      ? {
+          __data: value,
+          __expiresTime: Date.now() + time,
+        }
+      : value
+    Storage.prototype.setItem.call(localStorage, key, JSON.stringify(payload))
+  }
+  localStorage.getItem = function (key) {
+    const value = Storage.prototype.getItem.call(localStorage, key)
+    try {
+      const jsonVal = JSON.parse(value)
+      if (jsonVal.__expiresTime) {
+        return jsonVal.__expiresTime >= Date.now() ? JSON.stringify(jsonVal.__data) : void 0
+      }
+      return value
+    } catch (error) {
+      return value
+    }
+  }
+})()
